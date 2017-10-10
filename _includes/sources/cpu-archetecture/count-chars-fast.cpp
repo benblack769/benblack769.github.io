c066c80e3b9b93b77e2c26f011fd6be867f2f4ad
Ben Black <weepingwillowben@gmail.com>
2017-10-10 10:45:01 -0700

    fixed code

--- _includes/sources/cpu-archetecture/count-chars-fast.cpp
+++ _includes/sources/cpu-archetecture/count-chars-fast.cpp
@@ -5,16 +5,17 @@ using namespace std;
 int count_chars(const string & s,char cmp){
     int count = 0;
     constexpr int maxcount = 255;
+    const int str_size = s.size();
     int i = 0;
-    for(; i <= s.size()-maxcount; i += maxcount){
+    for(; i <= str_size-maxcount; i += maxcount){
         char inner_count = 0;
         for(int j = 0; j < maxcount; j++){
-            inner_count += (c == cmp);
+            inner_count += (s[i+j] == cmp);
         }
         count += inner_count;
     }
-    for(; i < s.size(); i += 1){
-        count += (c == cmp);
+    for(; i < str_size; i += 1){
+        count += (s[i] == cmp);
     }
     return count;
 }
