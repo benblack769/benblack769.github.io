484e2f9ab84f1c379d7ee92e74afbd77eba990a2
ben black <weepingwillowben@gmail.com>
2018-06-04 12:02:17 -0700

    updated aws client

--- link_only/aws_lambda_client/js/graph.js
+++ link_only/aws_lambda_client/js/graph.js
@@ -1,7 +1,4 @@
-AWS.config.region = 'us-west-2'; // Region
-AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: 'us-west-2:f6649357-417f-443c-8a4f-c853855d44ad',
-});
+var current_result;
 function checkposint(num_str){
     return /^(0|[1-9]\d*)$/.test(num_str)
 }
@@ -25,18 +22,18 @@ function verify_user_args(){
         $("#Pvalerr").show()
         retval = false
     }
-    if(!(checkposint(document.getElementById('RedSurvival').value) &&
+    /*if(!(checkposint(document.getElementById('RedSurvival').value) &&
          checkposint(document.getElementById('BlueSurvival').value))){
         $("#Survivalerr").show()
         retval = false
-    }
-    if(!(checkposint(document.getElementById('RedStart').value) &&
+    }*/
+    /*if(!(checkposint(document.getElementById('RedStart').value) &&
          checkposint(document.getElementById('BlueStart').value))){
         $("#Starterr").show()
         retval = false
-    }
+    }*/
     if(!(check_non_neg(document.getElementById('Aval').value))){
-        $("#Cvalerr").show()
+        $("#Avalerr").show()
         retval = false
     }
     if(!(check_num(document.getElementById('Cval').value))){
@@ -50,10 +47,11 @@ function hide_all(){
     $("#Nvalerr").hide()
     $("#Pvalerr").hide()
     $("#Survivalerr").hide()
-    $("#Starterr").hide()
+    //$("#Starterr").hide()
     $("#Cvalerr").hide()
 }
 function place_text_on_svg(text){
+    $("#downloadbutton").hide()
     document.getElementById("svg_item").innerHTML = text
 }
 function load_svg(){
@@ -69,43 +67,55 @@ function load_svg(){
         'Mval': document.getElementById('Mval').value,
         'Nval': document.getElementById('Nval').value,
         'Pval': document.getElementById('Pval').value,
+        'WithReplacement': document.getElementById('with_replacement').checked ? "true" : "false",
+        'DarkBlueSurv': document.getElementById('DarkBlueSurv').value,
+        'DarkRedSurv': document.getElementById('DarkRedSurv').value,
+        'LightBlueSurv': document.getElementById('LightBlueSurv').value,
+        'LightRedSurv': document.getElementById('LightRedSurv').value,
         'RedSurvival': document.getElementById('RedSurvival').value,
         'BlueSurvival': document.getElementById('BlueSurvival').value,
-        'RedStart': document.getElementById('RedStart').value,
-        'BlueStart': document.getElementById('BlueStart').value,
+        //'RedStart': document.getElementById('RedStart').value,
+        //'BlueStart': document.getElementById('BlueStart').value,
         'Aval': document.getElementById('Aval').value,
         'Cval': document.getElementById('Cval').value,
         'should_run_fast': document.getElementById('fast_checkbox').checked ? "true" : "false",
+        'use_single_trait': document.getElementById("single_trait_id").checked ? "true" : "false",
     }
-
-    console.log(result)
-    var pullParams = {
-      FunctionName : 'geneology_lambda_function',
-      InvocationType : 'RequestResponse',
-      LogType : 'None',
-      Payload: JSON.stringify(result),
-    };
-    var lambda = new AWS.Lambda({region: 'us-west-2', apiVersion: '2015-03-31'});
-    lambda.invoke(pullParams, function(error, data) {
-      if (error) {
-          place_text_on_svg("Server Error")
-        prompt(error);
-      } else {
-        data = JSON.parse(data.Payload);
-        if (data.success){
-            html_code = JSON.parse(data.success);
-            //console.log(html_code)
-
-            var container = document.getElementById("svg_item");
-            container.innerHTML = html_code;
-        }
-      }
-  });
+    current_result = result
+    get_svg(result)
     return true;
 }
-
+function download_data(filename, data) {
+    var blob = new Blob([data], {type: 'text/svg'});
+    if(window.navigator.msSaveOrOpenBlob) {
+        window.navigator.msSaveBlob(blob, filename);
+    }
+    else{
+        var elem = window.document.createElement('a');
+        elem.href = window.URL.createObjectURL(blob);
+        elem.download = filename;
+        document.body.appendChild(elem);
+        elem.click();
+        document.body.removeChild(elem);
+    }
+}
+function on_download_click(){
+    download_data($.param(current_result)+".svg",document.getElementById("svg_item").innerHTML)
+}
+function radio_change(){
+    document.getElementById("single_trait_id").onclick = function(){
+        $("#single_trait_survival").show()
+        $("#two_trait_survival").hide()
+    }
+    document.getElementById("two_trait_id").onclick = function(){
+        $("#single_trait_survival").hide()
+        $("#two_trait_survival").show()
+    }
+}
 $( document ).ready(function(){
     load_svg()
+    radio_change()
     console.log("lakfjlaksdjl")
     document.getElementById("submitbutton").onclick = load_svg
+    document.getElementById("downloadbutton").onclick = on_download_click
 })
